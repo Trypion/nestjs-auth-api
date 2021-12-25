525aef81f76dccea66bd49d9bd2205c7d1a60eba
Israel <israel.schmitt.j@gmail.com>
2021-12-25 19:14:28 -0300

    prisma added

---
+++ prisma/migrations/20211225211856_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "confirmationToken" TEXT,
+    "recoveryToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
